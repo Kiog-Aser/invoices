5a7714e1c852b725179e8589d3e57b586b90f9a4
Kiog-Aser <kiogaser@gmail.com>
2025-03-17 21:29:34 +0100

    improve app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'notifast.fun',
+          },
+        ],
+        destination: 'https://www.notifast.fun/:path*',
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
